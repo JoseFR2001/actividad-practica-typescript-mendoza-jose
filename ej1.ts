51fbc54c639014f38e8e98515bebfbeededc3fd4
JoseFR2001 <jose.rafael.mendoza.2001@egmail.com>
2026-08-11 14:33:09 -0300

    Primer ejercicio terminado

--- ej1.ts
+++ ej1.ts
@@ -19,8 +19,40 @@ class Producto {
   describir(): string {
     return `${this.nombre} (${this.categoria}): $${this.precio} - ${this.stock}`;
   }
+
+  hayStock(cantidad: number): boolean {
+    const existencia = this.stock >= cantidad;
+    return existencia;
+  }
+
+  venderUnidades(cantidad: number): void {
+    if (!this.hayStock(cantidad)) {
+      return console.log("No hay suficiente stock");
+    }
+
+    this.stock = this.stock - cantidad;
+    console.log(this.stock);
+  }
+
+  aplicarDescuento(porcentaje: number): number {
+    if (porcentaje < 0 || porcentaje > 100) {
+      throw new Error("Porcentaje no valido");
+    }
+
+    this.precio = this.precio * ((100 - porcentaje) / 100);
+
+    return this.precio;
+  }
 }
 
 const producto = new Producto("mate", 5000, "casa", 5);
+
+console.log(producto);
+
+console.log(producto.hayStock(5));
+
+producto.venderUnidades(4);
+
+console.log(producto.aplicarDescuento(10));
 
 console.log(producto);
